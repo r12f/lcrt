b322d5887c2427567615586ae203896ff3dc0139
r12f <r12f.code@gmail.com>
2023-02-13 13:58:35 -0800

    Adding lc_vecvec macro.

--- lcrt/src/lib.rs
+++ lcrt/src/lib.rs
@@ -2,8 +2,10 @@ mod guess_master;
 mod list;
 mod point;
 mod tree;
+mod vec;
 
 pub use guess_master::*;
 pub use list::*;
 pub use point::*;
 pub use tree::*;
+pub use vec::*;
---
+++ lcrt/src/vec.rs
@@ -0,0 +1,18 @@
+#[cfg(feature = "testing")]
+#[macro_export]
+macro_rules! lc_vecvec {
+    ($($e:tt),*) => {
+        vec![$($e.to_vec()),*]
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn lc_vecvec_macro_can_generate_vec_vec() {
+        let l = lc_vecvec![[9], [3, 15], [20], [7]];
+        assert_eq!(l, vec![vec![9], vec![3, 15], vec![20], vec![7]]);
+    }
+}
