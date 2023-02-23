dd7b985bb1183a2345d797f664fa64b4cfb6ce45
r12f <r12f.code@gmail.com>
2023-02-23 01:29:33 -0800

    string vec macro.

--- lcrt/src/vec.rs
+++ lcrt/src/vec.rs
@@ -1,3 +1,17 @@
+/// Macro for generate a vector of string with vec-like syntax.
+///
+/// Example:
+/// ```
+/// lc_strvec![["eat","tea","tan","ate","nat","bat"]]; // This creates a Vec<Vec<String>>.
+/// ```
+#[cfg(feature = "testing")]
+#[macro_export]
+macro_rules! lc_strvec {
+    ($($e:tt),*) => {
+        vec![$($e.to_string()),*]
+    };
+}
+
 /// Macro for generate a vector inside of a vector with vec-like syntax.
 ///
 /// Example:
@@ -15,6 +29,22 @@ macro_rules! lc_vecvec {
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
+
+    #[test]
+    fn lc_strvec_macro_can_generate_string_vec() {
+        let l = lc_strvec!["eat", "tea", "tan", "ate", "nat", "bat"];
+        assert_eq!(
+            l,
+            vec![
+                "eat".to_string(),
+                "tea".to_string(),
+                "tan".to_string(),
+                "ate".to_string(),
+                "nat".to_string(),
+                "bat".to_string()
+            ]
+        );
+    }
 
     #[test]
     fn lc_vecvec_macro_can_generate_vec_vec() {
